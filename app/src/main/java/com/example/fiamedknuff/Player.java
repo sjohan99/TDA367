8f66f31361cfe73bd1eb0dc15178cef640d574f4
Amanda <amanda.cyren@hotmail.se>
2021-09-15 16:01:38 +0200

    Lista med pieces skapad

--- app/src/main/java/com/example/fiamedknuff/Player.java
+++ app/src/main/java/com/example/fiamedknuff/Player.java
@@ -1,4 +1,9 @@
 package com.example.fiamedknuff;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Player {
+
+    private final ArrayList<Piece> pieces = new ArrayList<Piece>();
 }
