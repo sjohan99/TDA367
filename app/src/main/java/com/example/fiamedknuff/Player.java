389617e03499c79b9d49ac8f964b429fee01dd5a
Amanda <amanda.cyren@hotmail.se>
2021-09-15 17:54:25 +0200

    Skapat en variabel för namn i Player och en set-metod för denna

--- app/src/main/java/com/example/fiamedknuff/Player.java
+++ app/src/main/java/com/example/fiamedknuff/Player.java
@@ -11,11 +11,23 @@ import java.util.ArrayList;
 
 public class Player {
 
+    private String name;
     private final ArrayList<Piece> pieces;
 
-    public Player() {
+    public Player(String name) {
+        this.name = name;
         this.pieces = new ArrayList<Piece>(4);
     }
+
+    /**
+     * This set method sets the name of the player
+     *
+     *  @param name takes in a String as name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
     public ArrayList<Piece> getPieces() {
         return pieces;
