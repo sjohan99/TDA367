d2e571dfdb522cc4f22726ff328649d942d36e47
Amanda <amanda.cyren@hotmail.se>
2021-09-16 13:05:42 +0200

    Ändrat pieces till Collection från List

--- app/src/main/java/com/example/fiamedknuff/Player.java
+++ app/src/main/java/com/example/fiamedknuff/Player.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Player {
 
     private String name;
-    private final List<Piece> pieces;
+    private final Collection<Piece> pieces;
 
     public Player(String name) {
         this.name = name;
