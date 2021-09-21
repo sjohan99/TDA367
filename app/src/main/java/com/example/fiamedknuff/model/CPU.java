2d8916185f0913ed1ac5ecf6a6bdd897f966d519
Amanda <amanda.cyren@hotmail.se>
2021-09-21 09:51:38 +0200

    Skapat konstruktor för klassen CPU och påbörjat Javadoc

--- app/src/main/java/com/example/fiamedknuff/model/CPU.java
+++ app/src/main/java/com/example/fiamedknuff/model/CPU.java
@@ -1,7 +1,18 @@
 package com.example.fiamedknuff.model;
 
+/**
+ * A class CPU that creates a CPU object and couples computer player logic with a player
+ *
+ * Created by
+ * @author Amanda Cyrén
+ */
 public class CPU {
 
-    private Player player;
+    // A variable for the player object connected to the CPU
+    private final Player player;
+
+    public CPU(Player player) {
+        this.player = player;
+    }
 
 }
