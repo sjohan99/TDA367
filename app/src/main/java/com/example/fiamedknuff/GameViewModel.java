98d9c2acbfc589407d8f956117908b393f35ac5e
EmmaStalberg <emma.stalberg98@gmail.com>
2021-09-21 09:40:37 +0200

    Skapat ett första "skal" till en metod play i klassen GameViewModel

---
+++ app/src/main/java/com/example/fiamedknuff/GameViewModel.java
@@ -0,0 +1,26 @@
+package com.example.fiamedknuff;
+
+/**
+ * A class gameViewModel that ...
+ *
+ * Created by
+ * @author Emma Stålberg
+ */
+
+public class GameViewModel {
+
+    public void play() {
+        while() {
+            // For a new round…
+            // roll dice in the dice class. Returns roll.
+            // Get player pieces for current player
+            // Wait for player input
+            // Move selected player piece
+            // ...
+            // Select new player
+        }
+    }
+
+
+
+}
