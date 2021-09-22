e1bb9de0e4082639236ed2161e1ca2ad71d62669
Philip Winsnes <philip.winsnes@icloud.com>
2021-09-22 17:01:54 +0200

    Fixat static-fel i GameViewModel

--- app/src/main/java/com/example/fiamedknuff/model/GameFactory.java
+++ app/src/main/java/com/example/fiamedknuff/model/GameFactory.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class GameFactory {
 
-    private ArrayList<Player> players;
+    static private ArrayList<Player> players;
 
-    public Game createNewGame(String[] playerNames, Color[] colors) {
+    static public Game createNewGame(String[] playerNames, Color[] colors) {
         players = new ArrayList<>();
         // TODO: Check valid amount
         for (int i = 0; i < playerNames.length; i++) {
