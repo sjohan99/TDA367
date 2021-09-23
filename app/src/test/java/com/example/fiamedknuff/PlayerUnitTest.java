6652ed9a9fb034cacace5e75fdc6251288ddd4de
Amanda <amanda.cyren@hotmail.se>
2021-09-23 16:31:57 +0200

    Skapat en metod i PlayerUnitTest som testar metoden isMovable

--- app/src/test/java/com/example/fiamedknuff/PlayerUnitTest.java
+++ app/src/test/java/com/example/fiamedknuff/PlayerUnitTest.java
@@ -1,7 +1,30 @@
 package com.example.fiamedknuff;
 
+import com.example.fiamedknuff.model.Color;
+import com.example.fiamedknuff.model.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.assertFalse;
+
 public class PlayerUnitTest {
 
+    Player player;
+    String name;
+    Color color;
 
+    @Before
+    public void createPlayer() {
+        name = "TestPerson";
+        color = Color.BLUE;
+        player = new Player(name, color);
+
+    }
+
+    @Test
+    public void testIsNotMovable() {
+        //assertFalse(player.isMovable(player.getPieces().get(0), 3));
+    }
 
 }
