4c9b8cebc9c97b03afd959ac30e67b2d57d76974
Amanda <amanda.cyren@hotmail.se>
2021-09-24 20:00:30 +0200

    Skapat klassen rulebookFragment och påbörjat javadoc

---
+++ app/src/main/java/com/example/fiamedknuff/rulebookFragment.java
@@ -0,0 +1,11 @@
+package com.example.fiamedknuff;
+
+/**
+ * A class rulebookFragment that handles the GUI for the rule book
+ *
+ * Created by
+ * @author Amanda Cyrén
+ */
+
+public class rulebookFragment {
+}
