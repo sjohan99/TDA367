b70db338d3ee91d57f8bf34cd3f9412c0fe273a7
Amanda <amanda.cyren@hotmail.se>
2021-09-24 22:32:21 +0200

    Skapat metoden onCreateView i rulebookFragment med utkommenterade rader pga biblioteket som används inte har implementerats än

--- app/src/main/java/com/example/fiamedknuff/rulebookFragment.java
+++ app/src/main/java/com/example/fiamedknuff/rulebookFragment.java
@@ -1,9 +1,13 @@
 package com.example.fiamedknuff;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,6 +28,28 @@ public class rulebookFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_rule_book, container, false);
+
+        // Sets up the PageCurlView
+        //pageCurlView = (PageCurlView) view.findViewById(R.id.pagecurlView);
+
+        // Initialize an list with images to display in the rule book
+        images = new ArrayList<>();
+        images.add(R.drawable.pages); // First Image
+        images.add(R.drawable.pages); // Second Image
+        images.add(R.drawable.pages); // Third Image
+
+        // Adds the images to the PageCurlView and sets the speed of the page curl
+        //pageCurlView.setCurlView(images);
+        //pageCurlView.setCurlSpeed(600); // Set the speed in ms
+
+        return view;
     }
 
 }
