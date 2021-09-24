c7a705bd43e0429b21a81132d89a458c9d82c29a
Amanda <amanda.cyren@hotmail.se>
2021-09-24 22:25:08 +0200

    Skapat en variabel för en lista med bilder samt ett attribut för en PageCurlView i rulebookFragment

--- app/src/main/java/com/example/fiamedknuff/rulebookFragment.java
+++ app/src/main/java/com/example/fiamedknuff/rulebookFragment.java
@@ -1,5 +1,9 @@
 package com.example.fiamedknuff;
 
+import androidx.fragment.app.Fragment;
+
+import java.util.List;
+
 /**
  * A class rulebookFragment that handles the GUI for the rule book
  *
@@ -7,5 +11,12 @@ package com.example.fiamedknuff;
  * @author Amanda Cyrén
  */
 
-public class rulebookFragment {
+public class rulebookFragment extends Fragment {
+
+    // A list that holds the images to be displayed in the rule book
+    List<Integer> images;
+
+    // A PageCurlView that creates a page curl effect when turning pages
+    //PageCurlView pageCurlView;
+
 }
