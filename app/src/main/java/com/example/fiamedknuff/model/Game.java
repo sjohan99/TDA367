d88b7efdfcbc35f354f879222577ad0529d7065a
Amanda <amanda.cyren@hotmail.se>
2021-09-28 11:44:47 +0200

    Ändrat från player till getCurrentPlayer i metoden removeFinishedPiece i Game

--- app/src/main/java/com/example/fiamedknuff/model/Game.java
+++ app/src/main/java/com/example/fiamedknuff/model/Game.java
@@ -66,7 +66,7 @@ public class Game {
 
     private void removeFinishedPiece(Piece piece) {
         board.getPiecePositionHashMap().remove(piece);
-        player.removePiece(piece);
+        getCurrentPlayer().removePiece(piece);
     }
 
     private boolean isFinishedPlayer(Player player) {
