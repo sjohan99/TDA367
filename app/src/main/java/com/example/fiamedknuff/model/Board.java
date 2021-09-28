4b72261e330799340ca1e12d2899852f8efffebc
Hanna Boquist <boquist@student.chalmers.se>
2021-09-28 11:30:52 +0200

    Småfix, "throws exception" osv

--- app/src/main/java/com/example/fiamedknuff/model/Board.java
+++ app/src/main/java/com/example/fiamedknuff/model/Board.java
@@ -66,7 +66,6 @@ public class Board {
      */
 
     void movePiece(int roll, Piece piece) throws Exception {
-
         Position p;
         if (piece.isHome()) {
             p = new Position(10 + roll);    //ytterst preliminärt
@@ -81,10 +80,8 @@ public class Board {
         if (isOccupied(p)) {
             knockout(p);
         }
-
         piecePositionHashMap.put(piece,p);
     }
-
 
     Piece pieceAtPosition(Position pos) throws Exception {
 
@@ -122,6 +119,4 @@ public class Board {
         piecePositionHashMap.put(piece, positions.get(indexOfHomeNumber(piece)));
 
     }
-
-
 }
