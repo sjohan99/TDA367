6e7b834bbe78c05337bc47336e1c574baf02e604
Philip Winsnes <philip.winsnes@icloud.com>
2021-09-29 17:07:17 +0200

    Lagt till spelsidan och sidebaren.

--- app/src/main/java/com/example/fiamedknuff/fragments/GameViewFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/GameViewFragment.java
@@ -10,19 +10,12 @@ import android.view.ViewGroup;
 
 import com.example.fiamedknuff.R;
 
-/**
- * A class leaderboardFragment that ...
- *
- * Created by
- * @author Emma Stålberg
- */
-
-public class leaderboardFragment extends Fragment {
+public class GameViewFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        return inflater.inflate(R.layout.fragment_game_view, container, false);
     }
 }
