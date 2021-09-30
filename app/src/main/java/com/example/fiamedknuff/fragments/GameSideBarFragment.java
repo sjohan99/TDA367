e32f961263da0aaaf3e1b3903b0ecb083bda19e6
Philip Winsnes <philip.winsnes@icloud.com>
2021-09-30 15:43:46 +0200

    Justerat storleken på sidopanelen, skapat navigering från sidopanel till regelboken (återvändning till spelet fungerar). Appen visas nu i fullskärmsläge.

--- app/src/main/java/com/example/fiamedknuff/fragments/GameSideBarFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/GameSideBarFragment.java
@@ -1,21 +1,100 @@
 package com.example.fiamedknuff.fragments;
 
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.fiamedknuff.R;
 
+/**
+ * UI controller for the side bar layout.
+ * @author Philip Winsnes
+ */
+
 public class GameSideBarFragment extends Fragment {
+
+    private NavController navController;
+    private Button homeBtn, settingsBtn, rulebookBtn, soundBtn, replayBtn;
+
+    private boolean soundMuted;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_side_bar, container, false);
+        navController = NavHostFragment.findNavController(this);
+
+        View view = inflater.inflate(R.layout.fragment_game_side_bar, container, false);
+
+        initButtons(view);
+        specifyOnClickActions();
+
+        return view;
+    }
+
+    private void specifyOnClickActions() {
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
+
+        rulebookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+                navController.navigate(
+                        R.id.action_gameView_to_RulebookFragment,
+                        null,
+                        new NavOptions.Builder()
+                                .setEnterAnim(android.R.animator.fade_in)
+                                .setExitAnim(android.R.animator.fade_out)
+                                .build()
+                );
+            }
+        });
+
+        soundBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (soundMuted)
+                    soundBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_greenpos, 0, 0, 0);
+                else
+                    soundBtn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_redpos, 0, 0, 0);
+                soundMuted = !soundMuted;
+            }
+        });
+
+        replayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
+    }
+
+    private void initButtons(View view) {
+        homeBtn = view.findViewById(R.id.homeBtn);
+        settingsBtn = view.findViewById(R.id.settingsBtn);
+        rulebookBtn = view.findViewById(R.id.rulebookBtn2);
+        soundBtn = view.findViewById(R.id.soundBtn);
+        replayBtn = view.findViewById(R.id.replayBtn);
     }
 }
