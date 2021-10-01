f567ff43556353a7d36f66b818c08a0b2687d42b
Hanna Boquist <boquist@student.chalmers.se>
2021-10-01 09:50:05 +0200

    In progress, anteckningar i CPU-klassen. Behöver bestämma mer i domänmodellen hur vi gör med beroenden osv hur CPU-klassen ska få tillgång till det den behöver.

--- app/src/main/java/com/example/fiamedknuff/model/CPU.java
+++ app/src/main/java/com/example/fiamedknuff/model/CPU.java
@@ -1,5 +1,9 @@
 package com.example.fiamedknuff.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * A class CPU that creates a CPU object and couples computer player logic with a player
  *
@@ -15,4 +19,22 @@ public class CPU extends Player {
         super(name, color);
     }
 
+    void makeMove(int roll) {
+        Collection<Piece> movablePieces = new ArrayList<>();
+        movablePieces = getMovablePieces(pieces, roll);
+        //... HashMap?
+    }
+
+    /*
+    * Ta reda på vilka drag som är möjliga, som en vanlig Player -> getMovablePieces
+    * Rangordning på möjliga drag:
+    * 1. Om isOccupied -> knockput
+    * 2. Gå ut med en pjäs
+    * 3. Gå ut från bo med pjäs -> 1a eller 6a
+    * 4. Inget speciellt, bara gå antal steg på tärningen framåt. Slumpa mellan dessa om det finns fler?
+    *
+    * Välja vilken pjäs -> Flytta den
+    *
+    * Överlag: Kan inte flytta -> Gå vidare till nästa
+     */
 }
