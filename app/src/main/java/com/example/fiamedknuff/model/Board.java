ca07c99d8cb52462c8f6786149a7b1a2ea23b868
Johan Selin <johanselin99@gmail.com>
2021-10-02 18:36:57 +0200

    Logik för hur en pjäs ska flyttas kan nu hantera att gå över "varvs-linjen", gå ut på rätt plats, och gå in mot mitten vid rätt tillfälle. Även en del refaktorisering

--- app/src/main/java/com/example/fiamedknuff/model/Board.java
+++ app/src/main/java/com/example/fiamedknuff/model/Board.java
@@ -12,11 +12,16 @@ public class Board implements Serializable {
     private List<Position> positions; // List of all positions on the board including home-positions
     private HashMap<Piece, Position> piecePositionHashMap; // Maps the pieces to their positions
     private final int[] numberOfPositions = {0, 0, 0, 57, 0, 0, 0}; // Number of positions for each board size
+    private final int[] walkOutOffset = {0, 0, 0, 10, 0, 0, 0};
+    private final int[] lapLength = {0, 0, 0, 39, 0, 0, 0};
+    private final int playerCount;
 
     public Board(int playerCount, List<Piece> pieces) throws NotImplementedException {
         if (playerCount == 4) {
-            this.positions = createPositionsList(playerCount);
+            this.playerCount = playerCount;
+            this.positions = createPositionsList();
             this.piecePositionHashMap = initPiecePositionHashmap(pieces);
+            assignPieceOffsets(pieces);
         }
         else {
             throw new NotImplementedException();
@@ -27,10 +32,9 @@ public class Board implements Serializable {
      * Generates the position list with Position-indices ranging from (-4 * playerCount) to (the amount of
      * positions on the board excluding home-positions - 1). playerCount = 4 would give Positions with
      * indices -16 to 56.
-     * @param playerCount number of players playing.
      * @return list with Positions with indices (-4 * playerCount) to (numberOfPositions - 1).
      */
-    private ArrayList<Position> createPositionsList(int playerCount) {
+    private ArrayList<Position> createPositionsList() {
         int negativeIndices = playerCount * -4;
         int positiveIndices = numberOfPositions[playerCount - 1];
         ArrayList<Position> positionArrayList = new ArrayList<>();
@@ -44,11 +48,32 @@ public class Board implements Serializable {
         return positions;
     }
 
+    /**
+     * For testing purposes only as of now
+     * @param piece
+     * @return
+     */
+    public Position getPositionOutsideHomeOf(Piece piece) {
+        return piecePositionHashMap.get(piece);
+    }
+
+    private void assignPieceOffsets(List<Piece> pieces) {
+        int multiplier = -1;
+        for (int i = 0; i < pieces.size(); i++) {
+            if (i % 4 == 0) {
+                multiplier++;
+            }
+            pieces.get(i).setOffset(multiplier * walkOutOffset[playerCount-1]);
+        }
+    }
+
     private HashMap<Piece, Position> initPiecePositionHashmap(List<Piece> pieces) {
         HashMap<Piece, Position> piecePositionHashMap = new HashMap<>();
         int i = 0;
         for (Piece piece : pieces) {
-            piecePositionHashMap.put(piece, positions.get(i++));
+            piecePositionHashMap.put(piece, positions.get(i));
+            piece.setHomeNumber(positions.get(i).getPos());
+            i++;
         }
         return piecePositionHashMap;
     }
@@ -58,26 +83,62 @@ public class Board implements Serializable {
     }
 
     /**
-     * Moves Piece forward with the amount of the dice roll
-     * @param roll is the value from the latest dice roll
+     * Moves Piece forward one step
      * @param piece is the piece to be moved
      */
-    void movePiece(int roll, Piece piece) throws Exception {
+    void movePiece(Piece piece) throws Exception {
         Position p;
         if (piece.isHome()) {
-            p = new Position(10 + roll);    //ytterst preliminärt
+            p = getFirstPositionOf(piece);
         }
-        else {
-            p = new Position(piecePositionHashMap.get(piece).getPos() + roll);
+        else if (pieceWantsToGoInward(piece)) {
+            p = getFirstInwardPositionOf(piece);
+        }
+        else if (pieceAboutToLap(piece)) {
+            p = getFirstPositionInLap();
+        }
+        else { // Move as per usual
+            p = IncrementPositionOf(piece);
         }
 
-        piecePositionHashMap.remove(piece);
-        piece.setIndex(piece.getIndex() + roll);
+        piecePositionHashMap.put(piece, p); // Updates value of key
+        piece.setIndex(piece.getIndex() + 1);
+    }
 
-        if (isOccupied(p)) {
-            knockout(p);
-        }
-        piecePositionHashMap.put(piece,p);
+    private Position IncrementPositionOf(Piece piece) {
+        return positions.get((4 * playerCount) + piecePositionHashMap.get(piece).getPos() + 1);
+    }
+
+    private Position getFirstPositionInLap() {
+        return positions.get(4*playerCount);
+    }
+
+    private Position getFirstInwardPositionOf(Piece piece) {
+        Position p;
+        int posIndex = (4*playerCount) + piecePositionHashMap.get(piece).getPos();
+        posIndex = getPieceJumpAmount(piece, posIndex);
+        p = positions.get(posIndex);
+        return p;
+    }
+
+    private Position getFirstPositionOf(Piece piece) {
+        return positions.get((4*playerCount) + piece.getOffset());
+    }
+
+    private boolean pieceAboutToLap(Piece piece) {
+        return piecePositionHashMap.get(piece) == positions.get((4 * playerCount) + lapLength[playerCount-1]);
+    }
+
+    private boolean pieceWantsToGoInward(Piece piece) {
+        return piece.getIndex() == lapLength[playerCount-1];
+    }
+
+    private int getPieceJumpAmount(Piece piece, int posIndex) {
+        double indicator = Math.abs(piece.getHomeNumber()) / 4;
+        indicator = Math.ceil(indicator);
+        int jump = (int) ((indicator * 4) - 4);
+        posIndex += jump;
+        return posIndex;
     }
 
     /**
@@ -93,6 +154,7 @@ public class Board implements Serializable {
                 return piece;
             }
         }
+        // TODO: 2021-10-01 More specific exception
         throw new Exception(); // -> skriv in rätt Exception här?
     }
 
@@ -117,7 +179,17 @@ public class Board implements Serializable {
                 return positions.indexOf(p);
             }
         }
+        // TODO: 2021-10-01 More specific exception
         throw new Exception(); // -> skriv in rätt Exception här?
+    }
+
+    void knockOutPieceIfOccupied(Piece piece) throws Exception {
+        Position pos = piecePositionHashMap.get(piece);
+        piecePositionHashMap.remove(piece);
+        if (isOccupied(pos)) {
+            knockout(pos);
+        }
+        piecePositionHashMap.put(piece, pos);
     }
 
     /**
@@ -130,7 +202,6 @@ public class Board implements Serializable {
         piecePositionHashMap.remove(piece);
         piece.setIndex(0);
         piecePositionHashMap.put(piece, positions.get(indexOfHomeNumber(piece)));
-
     }
 
     /**
