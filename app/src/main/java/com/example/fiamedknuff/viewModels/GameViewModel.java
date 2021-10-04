6c42a68336ba6c069ee39e9ea0dbdd0763c97b06
EmmaStalberg <emma.stalberg98@gmail.com>
2021-10-04 12:00:31 +0200

    Uppdaterat metoden pieceClicked, skrivit hjälpmetoder samt börjat med javadoc. Även börjat på metoden rollDice som anropas när användaren klickar på tärningen.

--- app/src/main/java/com/example/fiamedknuff/viewModels/GameViewModel.java
+++ app/src/main/java/com/example/fiamedknuff/viewModels/GameViewModel.java
@@ -64,22 +64,51 @@ public class GameViewModel {
         }
     }*/
 
-    //TODO
+    //TODO - is the code in the model correct so the lists are corresponding?
     //should have a parameter with the piece or index of piece or something ??
     // update so that it works for when a player has finished - or maybe it already does??
-    public void pieceClicked() {
+    /**
+     * TODO
+     * @param indexOfPiece is the index of the piece in the list of pieces in the view, which
+     *                     corresponds to the index of the piece in the list of pieces in the model
+     */
+    public void pieceClicked(int indexOfPiece) {
         // if the rolled dice is already used, we can´t move any piece before another roll has
         // been made
         if (!game.getDice().getIsUsed()) {
-            // check if its movable
             List<Piece> movablePieces = game.getMovablePieces(game.getCurrentPlayer());
-            // TODO how to compare a piece with the respective imageview?
-            // if (movablePieces.contains the piece)
-            if(true) {
-                //move
-                game.getDice().setIsUsed(true);
+            Piece clickedPiece = game.getAllPlayerPieces().get(indexOfPiece);
+            //checks if the clicked piece is movable
+            if(movablePieces.contains(clickedPiece)) {
+                move(clickedPiece);
+                checkIfRemoved(clickedPiece);
             }
             moveToNextPlayer();
+        }
+    }
+
+    /**
+     * Checks if the piece is removed from the board in the model. If so, the piece
+     * should be removed from the view as well.
+     * @param piece is the piece that should be checked.
+     */
+    private void checkIfRemoved(Piece piece) {
+        if (!game.getAllPlayerPieces().contains(piece)) {
+            // remove piece from view
+            // TODO how?? pieceClicked return true or false?? Dependency??
+        }
+    }
+
+    /**
+     * Moves the piece and tells the dice it has been used.
+     * @param piece is the piece that should be moved
+     */
+    private void move(Piece piece) {
+        try {
+            game.move(piece);
+            // TODO move piece in the view!!
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -87,5 +116,20 @@ public class GameViewModel {
         game.selectNextPlayer();
         // check if game finished - finish
         // move dice to the next player
+        game.getDice().setIsUsed(true);
+    }
+
+    /**
+     * If the dice is ready to be rolled (i.e. is used), the dice is rolled.
+     * @return the value of the rolled dice (if it´s ready to be rolled), or -1 if
+     * it´s not ready to be rolled.
+     */
+    public int rollDice() {
+        // if the dice is used, you may roll again
+        if (game.getDice().getIsUsed()) {
+            game.rollDice();
+            return game.getDice().getRolledValue();
+        }
+        return -1;
     }
 }
