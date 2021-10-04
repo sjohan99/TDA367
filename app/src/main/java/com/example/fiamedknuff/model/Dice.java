a6711b227b801f84fcfd831247621c5fe591b7e6
EmmaStalberg <emma.stalberg98@gmail.com>
2021-10-04 09:58:31 +0200

    Skapat en variabel i klassen Dice som visar om ett tärningsslag har använts eller inte.

--- app/src/main/java/com/example/fiamedknuff/model/Dice.java
+++ app/src/main/java/com/example/fiamedknuff/model/Dice.java
@@ -15,21 +15,36 @@ public class Dice {
     private int valueMax;
     private Random rand = new Random();
 
+    // set to true when used and are waiting to be rolled, false if it is rolled and not used yet
+    private Boolean isUsed;
 
     public Dice(int valueMax) {
         this.valueMax = valueMax;
+        this.isUsed = true;
     }
 
     public Dice() {
         this.valueMax = 6;
+        this.isUsed = true;
+    }
+
+    public Boolean getIsUsed() {
+        return this.isUsed;
+    }
+
+    public void setIsUsed(Boolean isUsed) {
+        this.isUsed = isUsed;
     }
 
     /**
      * Rolls the dice and returns the value of the rolled dice. The value
-     * is between 1 and the maximum value of the dice.
+     * is between 1 and the maximum value of the dice. Sets the variable isUsed to false.
      * @return the value of the rolled dice
      */
     int rollDice() {
+        isUsed = false;
         return rand.nextInt(valueMax) + 1;
     }
+
+
 }
