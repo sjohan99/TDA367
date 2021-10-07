c9241e9af07e80bd51af7427cd475baa69d6eaff
Philip <phiwin@student.chalmers.se>
2021-10-07 13:42:45 +0200

    Uppdaterat GameViewModel

--- app/src/main/java/com/example/fiamedknuff/viewModels/GameViewModel.java
+++ app/src/main/java/com/example/fiamedknuff/viewModels/GameViewModel.java
@@ -69,6 +69,104 @@ public class GameViewModel extends ViewModel {
             }
         }
     }
+    
+    // TODO update so that it works for when a player has finished with the new method
+
+    /**
+     * TODO
+     * @param clickedPiece is the clicked piece
+     */
+    
+    public void pieceClicked(Piece clickedPiece) {
+        // if the rolled dice is already used, we can´t move any piece before another roll has
+        // been made
+        if (!game.getDice().getIsUsed()) {
+            List<Piece> movablePieces = game.getMovablePieces(game.getCurrentPlayer());
+            //checks if the clicked piece is movable
+            if(movablePieces.contains(clickedPiece)) {
+                move(clickedPiece);
+                checkIfRemoved(clickedPiece); // TODO change! see above
+            }
+            moveToNextPlayer();
+        }
+    }
+
+    /**
+     * TODO - should be erased and replaced
+     * Checks if the piece is removed from the board in the model. If so, the piece
+     * should be removed from the view as well.
+     * @param piece is the piece that should be checked.
+     */
+
+    private void checkIfRemoved(Piece piece) {
+        if (!game.getAllPlayerPieces().contains(piece)) {
+            // remove piece from view
+        }
+    }
+
+    /**
+     * Moves the piece and tells the dice it has been used.
+     * @param piece is the piece that should be moved
+     */
+
+    private void move(Piece piece) {
+        try {
+            game.move(piece);
+            // TODO move piece in the view!! (maybe new method... is coming)
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // TODO
+
+    private void moveToNextPlayer() {
+        game.selectNextPlayer();
+        // check if game finished - finish
+        // move dice to the next player
+        game.getDice().setIsUsed(true);
+
+    public LiveData<String> getPlayerName(int index) {
+        MutableLiveData<String> data = new MutableLiveData<>();
+        data.setValue(playerNames.get(index));
+        return data;
+    }
+
+    /**
+     * If the dice is ready to be rolled (i.e. is used), the dice is rolled.
+     * @return the value of the rolled dice (if it´s ready to be rolled), or -1 if
+     * it´s not ready to be rolled.
+     */
+
+    public int rollDice() {
+        
+        // if the dice is used, you may roll again
+
+        if (game.getDice().getIsUsed()) {
+            game.rollDice();
+            return game.getDice().getRolledValue();
+        }
+        return -1;
+    }
+
+    /**
+     * Returns all player pieces from game.
+     * @return all player pieces
+     */
+
+    public List<Piece> getPieces() {
+        return game.getAllPlayerPieces();
+    }
+
+    /**
+     * Returns the positions from the board.
+     * @return the positions from the board.
+     */
+
+    public List<Position> getPositions() {
+        return game.getPositions();
+    }
+}
 
     public LiveData<String> getPlayerName(int index) {
         MutableLiveData<String> data = new MutableLiveData<>();
