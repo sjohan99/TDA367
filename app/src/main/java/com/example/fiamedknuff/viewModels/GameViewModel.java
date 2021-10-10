58f3708ff625a45be5568a137f62d751128d7c5b
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2021-10-10 12:29:11 +0200

    Ändrat så det blev rätt ordning på spelarna. Småfix.

--- app/src/main/java/com/example/fiamedknuff/viewModels/GameViewModel.java
+++ app/src/main/java/com/example/fiamedknuff/viewModels/GameViewModel.java
@@ -1,5 +1,7 @@
 package com.example.fiamedknuff.viewModels;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -138,6 +140,7 @@ public class GameViewModel extends ViewModel {
      */
     public void selectNextPlayer() {
         game.selectNextPlayer();
+        Log.d(null, "nuvarande spelare: " + game.getCurrentPlayer().getName());
     }
 
     /**
