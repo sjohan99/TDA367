da1448f926d2cf17456eb4b09427b823fc3e3702
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2021-10-11 17:59:39 +0200

    Skapat en metod för att observera den spelare vars tur det är. Skall användas för att flytta tärningen till rätt spelare.

--- app/src/main/java/com/example/fiamedknuff/fragments/GameViewFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/GameViewFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.fiamedknuff.R;
+import com.example.fiamedknuff.model.Player;
 import com.example.fiamedknuff.viewModels.GameViewModel;
 
 /**
@@ -40,10 +42,20 @@ public class GameViewFragment extends Fragment {
 
         initLabels(view);
         initFragments();
+        initObservers();
 
         showAllFragments();
 
         return view;
+    }
+
+    private void initObservers() {
+        gameViewModel.currentPlayer.observe(getActivity(), new Observer<>() {
+            @Override
+            public void onChanged(Player player) {
+                // TODO move diceframe to current player
+            }
+        });
     }
 
     private void showAllFragments() {
