cbb3173475ed1af0905c514c5ee32de99b4a25b1
Hanna Boquist <boquist@student.chalmers.se>
2021-10-12 19:22:59 +0200

    Metod för rollDice inuti DiceFragment + anrop till denna

--- app/src/main/java/com/example/fiamedknuff/fragments/DiceFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/DiceFragment.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.fiamedknuff.R;
@@ -35,6 +36,7 @@ public class DiceFragment extends Fragment {
         gameViewModel = new ViewModelProvider(getActivity()).get(GameViewModel.class);
 
         initDice();
+        initObservers();
 
         return view;
     }
@@ -46,13 +48,17 @@ public class DiceFragment extends Fragment {
         diceImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int rolledValue = gameViewModel.rollDice();
+                rollDice();
+            }
+        });
+    }
 
-                // If the dice is rolled, the dice should be rotated and
-                // the method diceRolled should be called.
-                if (rolledValue != -1) {
-                    rotateDice(rolledValue);
-                    gameViewModel.diceRolled();
+    private void initObservers() {
+        gameViewModel.CPUdiceRoll.observe(getActivity(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if (aBoolean) {
+                    rollDice();
                 }
             }
         });
@@ -69,6 +75,17 @@ public class DiceFragment extends Fragment {
         diceImages.add(R.drawable.dice4);
         diceImages.add(R.drawable.dice5);
         diceImages.add(R.drawable.dice6);
+    }
+
+    private void rollDice() {
+        int rolledValue = gameViewModel.rollDice();
+
+        // If the dice is rolled, the dice should be rotated and
+        // the method diceRolled should be called.
+        if (rolledValue != -1) {
+            rotateDice(rolledValue);
+            gameViewModel.diceRolled();
+        }
     }
 
     /**
