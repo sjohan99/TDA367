08b64690fb45d295020962bf0cf47e61b7bb3a15
Amanda <amanda.cyren@hotmail.se>
2021-10-14 16:00:00 +0200

    Gjort javadoc i Dice mer enhetlig med resternade kod.

--- app/src/main/java/com/example/fiamedknuff/model/Dice.java
+++ app/src/main/java/com/example/fiamedknuff/model/Dice.java
@@ -12,18 +12,16 @@ import java.util.Random;
 
 
 public class Dice implements Serializable{
-    private int valueMax;
-    private final Random rand = new Random();
 
-    // Set to true when used and are waiting to be rolled, false if it is rolled and not used yet
-    private Boolean isUsed;
-
-    // A variable to hold the value of the rolled dice
-    private int rolledValue;
+    private int valueMax; // The maximum value the dice can have
+    private final Random rand = new Random(); // A variable Random to acquire a random number for the dice roll
+    private Boolean isUsed; // Set to true when used and are waiting to be rolled, false if it is rolled and not used yet
+    private int rolledValue; // A variable to hold the value of the rolled dice
 
     /**
-     * Creates a dice with possible numbers from 1 to valueMax value.
-     * @param valueMax the highest number the dice can roll
+     * Constructor that creates a dice with possible numbers from 1 to valueMax value.
+     *
+     * @param valueMax the highest number the dice can roll.
      */
     public Dice(int valueMax) {
         this.valueMax = valueMax;
@@ -31,7 +29,7 @@ public class Dice implements Serializable{
     }
 
     /**
-     * Creates a dice with possible numbers from 1 to 6
+     * Constructor that creates a dice with possible numbers from 1 to 6.
      */
     public Dice() {
         this.valueMax = 6;
@@ -39,24 +37,27 @@ public class Dice implements Serializable{
     }
 
     /**
-     * Gets the isUsed boolean value
-     * @return true if dice has been used, else false
+     * Gets the isUsed boolean value.
+     *
+     * @return true if dice has been used, else false.
      */
     public Boolean getIsUsed() {
         return this.isUsed;
     }
 
     /**
-     * Sets isUsed variable according to argument
-     * @param isUsed the boolean value isUsed variable to be changed to
+     * Sets isUsed variable according to argument.
+     *
+     * @param isUsed the boolean value isUsed variable to be changed to.
      */
     public void setIsUsed(Boolean isUsed) {
         this.isUsed = isUsed;
     }
 
     /**
-     * Returns the number rolled by the dice
-     * @return rolled number
+     * Returns the number rolled by the dice.
+     *
+     * @return rolled number.
      */
     public int getRolledValue() {
         return rolledValue;
@@ -65,7 +66,8 @@ public class Dice implements Serializable{
     /**
      * Rolls the dice and returns the value of the rolled dice. The value
      * is between 1 and the maximum value of the dice. Sets the variable isUsed to false.
-     * @return the value of the rolled dice
+     *
+     * @return the value of the rolled dice.
      */
     int rollDice() {
         isUsed = false;
