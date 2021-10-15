e045fc422727943ebe19eb8fe14b4490b0091d57
Philip Winsnes <philip.winsnes@icloud.com>
2021-10-15 12:50:30 +0200

    Fortsatt med design

--- app/src/main/java/com/example/fiamedknuff/ExitGamePopUpFragment.java
+++ app/src/main/java/com/example/fiamedknuff/ExitGamePopUpFragment.java
@@ -59,6 +59,6 @@ public class ExitGamePopUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exit_game_pop_up, container, false);
+        return inflater.inflate(R.layout.fragment_exit_game_popup, container, false);
     }
 }
