d0a9c8573ae4d5b10ac306bcd18121b37a2c3baa
Hanna Boquist <boquist@student.chalmers.se>
2021-10-15 14:26:39 +0200

    Fixat så att alla positioner stämmer enligt positionslistan

--- app/src/test/java/com/example/fiamedknuff/model/CPUUnitTest.java
+++ app/src/test/java/com/example/fiamedknuff/model/CPUUnitTest.java
@@ -33,28 +33,58 @@ public class CPUUnitTest {
     }
 
     @Test
+    public void testChoosePieceToMovePieceInHomeKnockout() {
+        HashMap<Piece, Position> piecePositionHashMap = board.getPiecePositionHashMap();
+        Player player1 = players.get(0);
+
+        // Set position for piece to be knockout
+        Piece knockoutPiece = player1.getPieces().get(0);
+        knockoutPiece.setIndex(16);
+        Position knockoutPos = board.getPositions().get(16+15);
+        piecePositionHashMap.put(knockoutPiece, knockoutPos);
+
+        // Set position for anther piece
+        Piece secondPiece = player1.getPieces().get(1);
+        secondPiece.setIndex(23);
+        Position secondPos = board.getPositions().get(16+22);
+        piecePositionHashMap.put(secondPiece, secondPos);
+
+        // Set position for first CPU-piece
+        Piece cpuFirstPiece = CPU.getPieces().get(0);
+        cpuFirstPiece.setIndex(7);
+        Position cpuFirstPos = board.getPositions().get(16+16);
+        piecePositionHashMap.put(cpuFirstPiece, cpuFirstPos);
+
+        // Set position for first CPU-piece
+        Piece cpuPieceToKnockout = CPU.getPieces().get(1);
+
+        assertThat(CPU.choosePieceToMove(6)).isEqualTo(cpuPieceToKnockout);
+    }
+
+    @Test
     public void testChoosePieceToMoveKnockout() {
         HashMap<Piece, Position> piecePositionHashMap = board.getPiecePositionHashMap();
         Player player1 = players.get(0);
 
         // Set position for piece to be knockout
         Piece knockoutPiece = player1.getPieces().get(0);
-        Position knockoutPos = new Position(10);
+        knockoutPiece.setIndex(16);
+        Position knockoutPos = board.getPositions().get(16+15);
         piecePositionHashMap.put(knockoutPiece, knockoutPos);
 
         // Set position for first CPU-piece
         Piece cpuFirstPiece = CPU.getPieces().get(0);
         cpuFirstPiece.setIndex(8);
-        Position cpuFirstPos = new Position(17);
+        Position cpuFirstPos = board.getPositions().get(16+17);
         piecePositionHashMap.put(cpuFirstPiece, cpuFirstPos);
 
         // Set position for second CPU-piece
         Piece cpuSecondPiece = CPU.getPieces().get(1);
         cpuSecondPiece.setIndex(4);
-        Position cpuSecondPos = new Position(13);
+        Position cpuSecondPos = board.getPositions().get(16+13);
         piecePositionHashMap.put(cpuSecondPiece, cpuSecondPos);
 
-        assertThat(CPU.choosePieceToMove(2)).isEqualTo(cpuFirstPiece);
+        assertThat(CPU.choosePieceToMove(2)).isEqualTo(cpuSecondPiece);
     }
 
    @Test
@@ -64,19 +94,19 @@ public class CPUUnitTest {
        // Set position for a CPU-piece
        Piece cpuFirstPiece = CPU.getPieces().get(0);
        cpuFirstPiece.setIndex(4);
-       Position cpuFirstPos = new Position(13);
+       Position cpuFirstPos = board.getPositions().get(16+13);
        piecePositionHashMap.put(cpuFirstPiece, cpuFirstPos);
 
        // Set position for a second cpu-piece
        Piece cpuSecondPiece = CPU.getPieces().get(1);
        cpuSecondPiece.setIndex(21);
-       Position cpuSecondPos= new Position(30);
+       Position cpuSecondPos= board.getPositions().get(16+30);
        piecePositionHashMap.put(cpuSecondPiece, cpuSecondPos);
 
        // Set position for third CPU-piece
        Piece cpuThirdPiece = CPU.getPieces().get(2);
        cpuThirdPiece.setIndex(board.getFinishIndex() - 2);
-       Position cpuToBeFinishPos = new Position(52);
+       Position cpuToBeFinishPos = board.getPositions().get(16+52);
        piecePositionHashMap.put(cpuThirdPiece, cpuToBeFinishPos);
 
        assertThat(CPU.choosePieceToMove(2)).isEqualTo(cpuThirdPiece);
@@ -88,19 +118,19 @@ public class CPUUnitTest {
        // Set position for a CPU-piece
        Piece cpuFirstPiece = CPU.getPieces().get(0);
        cpuFirstPiece.setIndex(4);
-       Position cpuFirstPos = new Position(13);
+       Position cpuFirstPos = board.getPositions().get(16+13);
        piecePositionHashMap.put(cpuFirstPiece, cpuFirstPos);
 
        // Set position for a second cpu-piece
        Piece cpuSecondPiece = CPU.getPieces().get(1);
        cpuSecondPiece.setIndex(21);
-       Position cpuSecondPos= new Position(30);
+       Position cpuSecondPos= board.getPositions().get(16+30);
        piecePositionHashMap.put(cpuSecondPiece, cpuSecondPos);
 
        // Set position for third CPU-piece
        Piece cpuThirdPiece = CPU.getPieces().get(2);
        cpuThirdPiece.setIndex(34);
-       Position cpuThirdPos = new Position(43);
+       Position cpuThirdPos = board.getPositions().get(16+43);
        piecePositionHashMap.put(cpuThirdPiece, cpuThirdPos);
 
        // Set position for fourth CPU-piece
@@ -115,19 +145,19 @@ public class CPUUnitTest {
         // Set position for first CPU-piece
         Piece cpuFirstPiece = CPU.getPieces().get(0);
         cpuFirstPiece.setIndex(4);
-        Position cpuFirstPos = new Position(13);
+        Position cpuFirstPos = board.getPositions().get(16+13);
         piecePositionHashMap.put(cpuFirstPiece, cpuFirstPos);
 
         // Set position for second cpu-piece (leading piece)
         Piece cpuSecondPiece = CPU.getPieces().get(1);
         cpuSecondPiece.setIndex(21);
-        Position cpuSecondPos= new Position(30);
+        Position cpuSecondPos= board.getPositions().get(16+30);
         piecePositionHashMap.put(cpuSecondPiece, cpuSecondPos);
 
-        // Set position for third CPU-piece
+        // Set position for third CPU-piece, (in homePath)
         Piece cpuThirdPiece = CPU.getPieces().get(2);
         cpuThirdPiece.setIndex(43);
-        Position cpuThirdPos = new Position(52);
+        Position cpuThirdPos = board.getPositions().get(16+52);
         piecePositionHashMap.put(cpuThirdPiece, cpuThirdPos);
 
         assertThat(CPU.choosePieceToMove(4)).isEqualTo(cpuSecondPiece);
@@ -150,34 +180,4 @@ public class CPUUnitTest {
     public void testChoosePieceToMoveNoMovablePieces() {
         assertThat(CPU.choosePieceToMove(3)).isEqualTo(null);
     }
-
-    @Test
-    public void testChoosePieceToMovePieceInHomeKnockout() {
-        HashMap<Piece, Position> piecePositionHashMap = board.getPiecePositionHashMap();
-        Player player1 = players.get(0);
-
-        // Set position for piece to be knockout
-        Piece knockoutPiece = player1.getPieces().get(0);
-        knockoutPiece.setIndex(16);
-        //Position knockoutPos = board.getFirstPositionIndexInLap();
-        //piecePositionHashMap.put(knockoutPiece, knockoutPos);
-
-        // Set position for anther piece
-        Piece secondPiece = player1.getPieces().get(1);
-        secondPiece.setIndex(23);
-        Position secondPos = board.getPositions().get(16+22);
-        piecePositionHashMap.put(secondPiece, secondPos);
-
-        // Set position for first CPU-piece
-        Piece cpuFirstPiece = CPU.getPieces().get(0);
-        cpuFirstPiece.setIndex(7);
-        Position cpuFirstPos = board.getPositions().get(16+16);
-        piecePositionHashMap.put(cpuFirstPiece, cpuFirstPos);
-
-        // Set position for first CPU-piece
-        Piece cpuPieceToKnockout = CPU.getPieces().get(1);
-
-        assertThat(CPU.choosePieceToMove(6)).isEqualTo(cpuPieceToKnockout);
-    }
-
 }
