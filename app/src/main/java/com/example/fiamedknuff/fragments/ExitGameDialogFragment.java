eff868463b9ecb9f3887cd937f11abdab786d4f0
Philip Winsnes <philip.winsnes@icloud.com>
2021-10-15 14:36:31 +0200

    Lagt till en animation för återkomsten till huvudmenyn

--- app/src/main/java/com/example/fiamedknuff/fragments/ExitGameDialogFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/ExitGameDialogFragment.java
@@ -2,12 +2,14 @@ package com.example.fiamedknuff.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.fiamedknuff.R;
@@ -36,13 +38,20 @@ public class ExitGameDialogFragment extends DialogFragment {
         return new AlertDialog.Builder(requireContext())
                 .setMessage(getString(R.string.exit_game_popup_message))
                 .setPositiveButton(getString(R.string.popup_cancel_option), (dialog, which) -> {})
-                .setNeutralButton(getString(R.string.popup_proceed_option), (dialog, which) -> {
-                    // TODO: Add return home logic
-                    navController.navigate(R.id.action_gameView_to_mainMenuFragment);
-                })
+                .setNeutralButton(getString(R.string.popup_proceed_option), this::onClick)
                 .create();
     }
 
     public static String TAG = "ExitGameDialogFragment";
 
+    private void onClick(DialogInterface dialog, int which) {
+        // TODO: 2021-10-15 Add more return logic if necessary
+        navController.navigate(R.id.action_gameView_to_mainMenuFragment,
+                null,
+                new NavOptions.Builder()
+            .setEnterAnim(android.R.animator.fade_in)
+            .setExitAnim(android.R.animator.fade_out)
+            .build()
+        );
+    }
 }
