b1cbfb994a0c6dda91f318e60e05cae46d985040
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2021-10-16 14:20:05 +0200

    Skapat en abstrakt klass BoardFragment. Detta för att man i framtiden ska kunna ha andra bräden än standardbrädet.

---
+++ app/src/main/java/com/example/fiamedknuff/fragments/BoardFragment.java
@@ -0,0 +1,12 @@
+package com.example.fiamedknuff.fragments;
+
+/**
+ * TODO write better
+ * An abstract class BoardFragment. Superclass to our fragments.
+ *
+ * Created by
+ * @author Emma Stålberg
+ */
+public abstract class BoardFragment {
+
+}
