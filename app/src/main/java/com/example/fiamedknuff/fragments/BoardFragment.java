8b12ca5005271786360ceafed554373e498552ef
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2021-10-17 18:27:17 +0200

    Skapat metoden OnCreateView, och skapat abstrakta metoder som behövs i den.

--- app/src/main/java/com/example/fiamedknuff/fragments/BoardFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/BoardFragment.java
@@ -1,12 +1,56 @@
 package com.example.fiamedknuff.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
 /**
- * TODO write better
+ * TODO write better comment
  * An abstract class BoardFragment. Superclass to our fragments of boards.
  *
  * Created by
  * @author Emma Stålberg
  */
-public abstract class BoardFragment {
+public abstract class BoardFragment extends Fragment {
+
+    View view;
+    boolean alreadyInitialized;
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        setView(inflater, container);
+        setGameViewModel();
+        setConstraintLayout();
+        initPositions();
+        initPieces();
+        // TODO: 2021-10-13 Check if everything should be inside if-clause
+        if (!alreadyInitialized) {
+            initObservers();
+            alreadyInitialized = true;
+        }
+
+        reInit();
+
+        return view;
+    }
+
+    protected abstract void setView(LayoutInflater inflater, ViewGroup container);
+
+    protected abstract void setGameViewModel();
+
+    protected abstract void setConstraintLayout();
+
+    protected abstract void initPositions();
+
+    protected abstract void initPieces();
+
+    protected abstract void initObservers();
+
+    protected abstract void reInit();
 
 }
