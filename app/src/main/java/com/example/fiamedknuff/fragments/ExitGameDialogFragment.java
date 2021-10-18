edc27e263a069dfb37cf6903f8843fd3ed272c14
Philip Winsnes <philip.winsnes@icloud.com>
2021-10-18 21:23:56 +0200

    Designat popup-fönstren och låtit de visas vid val i SideBar. Saknar funktionalitet men går att stänga ner genom att klicka utanför

--- app/src/main/java/com/example/fiamedknuff/fragments/ExitGameDialogFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/ExitGameDialogFragment.java
@@ -21,8 +21,6 @@ import com.example.fiamedknuff.R;
  */
 public class ExitGameDialogFragment extends DialogFragment {
 
-    private NavController navController;
-
     /**
      * Creates a Dialog to display as part of the DialogFragment.
      * The DialogFragment handles displaying the Dialog at appropriate
@@ -33,26 +31,12 @@ public class ExitGameDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        navController = NavHostFragment.findNavController(this);
         setCancelable(true);
         return new AlertDialog.Builder(requireContext())
-                .setMessage(getString(R.string.exit_game_popup_message))
-                .setPositiveButton(getString(R.string.popup_cancel_option), (dialog, which) -> {})
-                .setNeutralButton(getString(R.string.popup_proceed_option), this::onClick)
+                .setView(R.layout.fragment_exit_game_dialog)
                 .create();
     }
 
     public static String TAG = "ExitGameDialogFragment";
-
-    private void onClick(DialogInterface dialog, int which) {
-        // TODO: 2021-10-15 Add more return logic if necessary
-        navController.navigate(R.id.action_gameView_to_mainMenuFragment,
-                null,
-                new NavOptions.Builder()
-            .setEnterAnim(android.R.animator.fade_in)
-            .setExitAnim(android.R.animator.fade_out)
-            .build()
-        );
-    }
 
 }
