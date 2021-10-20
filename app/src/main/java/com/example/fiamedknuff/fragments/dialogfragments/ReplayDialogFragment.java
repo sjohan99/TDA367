4eb0b8b9c24f84eb8eaac73eb7b6ac160daba238
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2021-10-20 11:46:27 +0200

    Rensat upp i koden. Småfix. Fixat javadoc för klassen med "responsibility, used by, uses, author" enligt uppgift.

--- app/src/main/java/com/example/fiamedknuff/fragments/dialogfragments/ReplayDialogFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/dialogfragments/ReplayDialogFragment.java
@@ -3,13 +3,7 @@ package com.example.fiamedknuff.fragments.dialogfragments;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,8 +12,14 @@ import androidx.fragment.app.DialogFragment;
 import com.example.fiamedknuff.R;
 
 /**
- * A DialogFragment container, designed for creating and hosting dialogs (popups).
- * For more information: <a href="https://developer.android.com/guide/fragments/dialogs"> Displaying dialogs with DialogFragment </a>
+ * Responsibility: A DialogFragment container, designed for creating and hosting dialogs (popups).
+ *  For more information: <a href="https://developer.android.com/guide/fragments/dialogs">
+ *      Displaying dialogs with DialogFragment </a>
+ *
+ * Used by: GameSideBarFragment
+ * Uses: -
+ *
+ * Created by
  * @author Philip Winsnes
  */
 public class ReplayDialogFragment extends DialogFragment {
@@ -41,6 +41,7 @@ public class ReplayDialogFragment extends DialogFragment {
                 .create();
     }
 
+    //TODO javadoc
     public static String TAG = "ReplayDialogFragment";
 
 }
