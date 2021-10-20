688b25d4c014260e854caad9aea7d9f98f0647ae
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2021-10-20 11:42:54 +0200

    Rensat upp i koden. Småfix. Fixat javadoc för klassen med "responsibility, used by, uses, author" enligt uppgift.

--- app/src/main/java/com/example/fiamedknuff/fragments/dialogfragments/ExitGameDialogFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/dialogfragments/ExitGameDialogFragment.java
@@ -2,21 +2,24 @@ package com.example.fiamedknuff.fragments.dialogfragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.navigation.NavController;
-import androidx.navigation.NavOptions;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.fiamedknuff.R;
 
 /**
- * A DialogFragment container, designed for creating and hosting dialogs (popups).
- * For more information: <a href="https://developer.android.com/guide/fragments/dialogs"> Displaying dialogs with DialogFragment </a>
+ * TODO delete link or not?
+ * Responsibility: A DialogFragment container, designed for creating and hosting dialogs (popups).
+ * For more information: <a href="https://developer.android.com/guide/fragments/dialogs">
+ *     Displaying dialogs with DialogFragment </a>
+ *
+ * Used by: GameSideBarFragment
+ * Uses: -
+ *
+ * Created by
  * @author Philip Winsnes
  */
 public class ExitGameDialogFragment extends DialogFragment {
@@ -37,6 +40,8 @@ public class ExitGameDialogFragment extends DialogFragment {
                 .create();
     }
 
+
+    //TODO write javadoc
     public static String TAG = "ExitGameDialogFragment";
 
 }
