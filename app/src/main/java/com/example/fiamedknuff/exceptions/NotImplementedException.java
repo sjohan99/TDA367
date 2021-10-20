c142dda563cbdaf8fd51fb5cd9ae0fd2bca3fc96
EmmaStalberg <74003232+EmmaStalberg@users.noreply.github.com>
2021-10-20 10:25:36 +0200

    Rensat upp i koden. Fixat javadoc för klassen.

--- app/src/main/java/com/example/fiamedknuff/exceptions/NotImplementedException.java
+++ app/src/main/java/com/example/fiamedknuff/exceptions/NotImplementedException.java
@@ -1,5 +1,15 @@
 package com.example.fiamedknuff.exceptions;
 
+/**
+ * Responsibility: TODO
+ *
+ * Used by: GameSetupFragment, Board, Game, GameFactory, GameViewModel
+ * Uses: TODO
+ *
+ * Created by
+ * @author Johan Selin
+ */
+
 public class NotImplementedException extends Exception {
 
     public NotImplementedException(String errorMessage) {
