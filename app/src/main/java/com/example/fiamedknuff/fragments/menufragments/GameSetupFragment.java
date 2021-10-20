805ffba3190d9a7015f9b5b9111c8f7a64f5d27a
Hanna Boquist <boquist@student.chalmers.se>
2021-10-20 10:47:59 +0200

    TODO-kommentar

--- app/src/main/java/com/example/fiamedknuff/fragments/menufragments/GameSetupFragment.java
+++ app/src/main/java/com/example/fiamedknuff/fragments/menufragments/GameSetupFragment.java
@@ -91,6 +91,7 @@ public class GameSetupFragment extends Fragment {
         });
     }
 
+    // TODO: 2021-10-20 Fix CheckBox-problem with indexOfOutBounds 
     private void initPlayerAmountSpinner(View view) {
         playerAmountSpinner = view.findViewById(R.id.playerAmountSpinner);
         populateSpinner();
