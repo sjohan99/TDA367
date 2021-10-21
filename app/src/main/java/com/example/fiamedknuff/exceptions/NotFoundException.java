61b3c5cc63ff7a662fbf87ebe2701b3ac488133f
Johan Selin <johanselin99@gmail.com>
2021-10-21 13:30:18 +0200

    Javadocs för undantag

--- app/src/main/java/com/example/fiamedknuff/exceptions/NotFoundException.java
+++ app/src/main/java/com/example/fiamedknuff/exceptions/NotFoundException.java
@@ -1,7 +1,15 @@
 package com.example.fiamedknuff.exceptions;
 
+/**
+ * Exception for when iterating an iterative searching for a certain value but it cannot be found
+ * @author Johan Selin
+ */
 public class NotFoundException extends Exception {
 
+    /**
+     * Creates a NotFoundException with a message to the client
+     * @param errorMessage Specify what couldn't be found
+     */
     public NotFoundException(String errorMessage) {
         super(errorMessage);
     }
--- app/src/main/java/com/example/fiamedknuff/exceptions/NotImplementedException.java
+++ app/src/main/java/com/example/fiamedknuff/exceptions/NotImplementedException.java
@@ -1,5 +1,9 @@
 package com.example.fiamedknuff.exceptions;
 
+/**
+ * Exception for when the client has issued a valid command but the feature is not yet implemented
+ * @author Johan Selin
+ */
 public class NotImplementedException extends Exception {
 
     public NotImplementedException(String errorMessage) {
